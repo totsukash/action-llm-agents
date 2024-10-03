237a63a6aeb6772e2a9c0efc8e8b395f4a1e5e80
ShotaTotsuka <shota.totsuka@blueish.co.jp>
2024-10-03 09:02:01 +0900

    fix: Bot起動

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,19 @@ import { load } from 'ts-dotenv';
 const env = load({
   SLACK_BOT_TOKEN: String,
   SLACK_APP_TOKEN: String,
-  SLACK_SIGNING_SECRET: String,
   PORT: Number,
 });
 
 // Initialize SlackBot
 const app = new App({
   token: env.SLACK_BOT_TOKEN,
-  signingSecret: env.SLACK_SIGNING_SECRET,
   appToken: env.SLACK_APP_TOKEN,
+  socketMode: true,
 });
 
-console.log("SLACK_BOT_TOKEN", env.SLACK_BOT_TOKEN);
+app.event("app_mention", async ({ event, say }) => {
+  await say(`Hello, <@${event.user}>!`);
+});
 
 // Send message to SlackBot
 app.message('', async ({ message, say }) => {
